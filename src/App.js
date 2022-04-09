82c7215106e78a748dbaa56adeeec4240d9706a5
ibrahim gb <ibrahim.guoual.b@gmail.com>
2022-04-09 15:51:21 +0100

    finishing COMPRÉHENSION ORALE quizz section

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import QuizzSection from "./compoments/quizzSection/QuizzSection";
+
+function App() {
+    return (
+        <div className="App">
+            <QuizzSection />
+        </div>
+    );
+}
+
+export default App;
